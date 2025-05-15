c26ba4613cbd0d0c46ac603af0a3da64502b5679
Merdan-Mahmudow <softprank@gmail.com>
2025-05-16 00:52:52 +0300

    modified:   src/main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -19,27 +19,7 @@ declare module '@tanstack/react-router' {
   }
 }
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyB8pRk9U-DWZBkj_j525YQXaj5o6k_Nf24",
-  authDomain: "kimchistop-fb0a6.firebaseapp.com",
-  projectId: "kimchistop-fb0a6",
-  storageBucket: "kimchistop-fb0a6.firebasestorage.app",
-  messagingSenderId: "125767968875",
-  appId: "1:125767968875:web:6b4faf2efd8660cc4ba92e",
-  measurementId: "G-X97RNWT8CG"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 
 createRoot(document.getElementById('root')!).render(
